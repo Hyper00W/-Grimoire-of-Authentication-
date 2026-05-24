ee7981484e630d5ef4e24cb863e826a419b64666
Hyper00W <aadimanhas007@gmail.com>
2026-05-24 21:04:03 +0530

    Updated DataBase integrated

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from passlib.context import CryptContext 
-from database import engine
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
 from model import UserDB
 
 app = FastAPI()
+
 UserDB.metadata.create_all(bind=engine)
+
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class User(BaseModel):
     username: str
     email: str
     password: str
+    age: int
 
 def hash_password(password: str):
     return pwd_context.hash(password)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def home():
     return {"message": "Auth Service Running"}
 
 @app.post("/register")
-def register(user: User):
+def register(user: User, db: Session = Depends(get_db)):
     hashed_password = hash_password(user.password)
 
+    new_user = UserDB(
+        username = user.username,
+        email = user.email,
+        hashed_password = hashed_password,
+        age = user.age  
+    )
+
+    db.add(new_user)
+    db.commit()
+
     return {
-        "username": user.username,
-        "email": user.email,
-        "hashed_password": hashed_password,
-        "message": "User registered successfully"
+        "message" : "User Registered successfully"
     }
